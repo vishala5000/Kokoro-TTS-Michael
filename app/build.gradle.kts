9ebcb092e0679206c8c13d658ba0c6aa192bdd1e
Simple <157860608+vishala5000@users.noreply.github.com>
2026-09-13 07:16:28 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,78 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
 
+android {
+    namespace = "com.vishala.kokoromichael"
+
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.vishala.kokoromichael"
+
+        minSdk = 29
+        targetSdk = 35
+
+        versionCode = 1
+        versionName = "1.0"
+
+        vectorDrawables {
+            useSupportLibrary = false
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+
+            proguardFiles(
+                getDefaultProguardFile(
+                    "proguard-android-optimize.txt"
+                ),
+                "proguard-rules.pro"
+            )
+        }
+
+        debug {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    androidResources {
+        noCompress += listOf(
+            "onnx",
+            "bin"
+        )
+    }
+
+    packaging {
+        jniLibs {
+            useLegacyPackaging = false
+        }
+    }
+
+    sourceSets {
+        getByName("main") {
+            jniLibs.srcDirs(
+                "src/main/jniLibs"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation(
+        "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2"
+    )
+}
